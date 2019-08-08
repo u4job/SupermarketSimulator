82ba6dacaec8aeb150c7f4011ab86fe0a53118aa
b6976300@urhen.com <b6976300@urhen.com>
2019-08-08 17:30:09 +0300

    Add project files.

---
+++ Cashier.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace SupermarketSimulator
+{
+    public class Cashier
+    {
+        public string CashierNumber { get; set; }
+
+        public Cashier(int cashierNumber)
+        {
+            this.CashierNumber = cashierNumber.ToString();
+        }
+        
+        public void StartProcessing(int ProcessingTime) {
+            Console.WriteLine("Cashier {0} start process, it will take him {1} to process all prodacts.", this.CashierNumber, ProcessingTime.ToString());
+            Thread.Sleep(ProcessingTime * 1000);
+            Console.WriteLine("Cashier {0} finished to process all prodacts.", this.CashierNumber);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SupermarketSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfCashiers = 0;
+            int maxProcessingTime = 0;
+
+            //safely enter number of cashiers
+            do
+            {
+                Console.Write("Enter number of cashiers: ");
+                string strNumberOfCashiers = Console.ReadLine();
+
+                try
+                {
+                    numberOfCashiers = int.Parse(strNumberOfCashiers);
+                }
+                catch
+                { }
+            } while (numberOfCashiers <= 0);
+
+            //new cashier list
+            List<Cashier> cashierList = new List<Cashier>();
+
+            //add new cashiers into the list
+            for (int i = 0; i < numberOfCashiers; i++)
+            {
+                cashierList.Add(new Cashier(i + 1));
+            }
+
+            //safely enter maximum processing time
+            do
+            {
+                Console.Write("Enter maximum processing time(min 3): ");
+                string strMaxProcessingTime = Console.ReadLine();
+
+                try
+                {
+                    maxProcessingTime = int.Parse(strMaxProcessingTime);
+                }
+                catch
+                { }
+            } while (maxProcessingTime <= 3);
+
+            //new customers queue - every customer as random processing time
+            Queue<int> customers = new Queue<int>();
+
+            //to calculat the processing time of the customer
+            Random processTime = new Random();
+
+            //to close the Supermarket - no new customer will be add
+            bool SupermarketOpen = true;
+
+            //new thread that will add new customers every second
+            Thread AddCustomers = new Thread(delegate ()
+            {
+                while (SupermarketOpen)
+                {
+                    customers.Enqueue(processTime.Next(1, maxProcessingTime));
+                    Console.WriteLine("There is " + customers.Count + " customers is the queue.");
+                    Thread.Sleep(1000);
+                }
+            });
+
+            //new thread that will close the supermarket on any key
+            Thread closeSupermarket = new Thread(delegate ()
+            {
+                Console.Read();
+                SupermarketOpen = false;
+            });
+
+            //start the threads to add customers and close supermarket
+            AddCustomers.Start();
+            closeSupermarket.Start();
+
+            //start multiple threads for each cashiers
+            Parallel.For(0, cashierList.Count, (i) =>
+            {
+                //if supermarket open or there is still customers in the queue
+                while (SupermarketOpen || (SupermarketOpen == false && customers.Count != 0))
+                {
+                    //get the next customer from the queue if there is any
+                    int tempProcessTime = -1;
+                    try
+                    {
+                        tempProcessTime = customers.Dequeue();
+                    }
+                    catch { }
+
+                    if (tempProcessTime != -1)
+                    {
+                        cashierList[i].StartProcessing(tempProcessTime);
+                    }
+                }
+            });
+        }
+    }
+}
